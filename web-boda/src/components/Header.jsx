05540fc3d990a7c0eec760a300685a7e9eb1b33b
paujulbe <paujulbe@gmail.com>
2024-04-21 20:34:20 +0200

    react template

---
+++ web-boda/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+export const Header = () => {
+  return (
+    <div className='header'>
+        <a href="www.google.es">
+            Entra
+        </a>
+    </div>
+  )
+}
